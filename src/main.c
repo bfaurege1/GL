b40cb5d529a708073011f70c877a8363f22bed1b
bfaurege1 <baptiste.faure@u-psud.fr>
2018-11-14 18:42:28 +0100

    J'ai terminé Exo44

--- src/main.c
+++ src/main.c
@@ -1,9 +1,18 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
-#include "my_header.h"
+#include "TP_lib.h"
+#include "cvi_gui.h"
 
-int main(void)
+void main (void)
 {
-    return 0;
+	short resultat, i=0;
+	while (i<10)
+	{
+		resultat = LancerDe();
+		i++;
+	DisplayLong ("val=", resultat) ;
+	}
+	
+	
 }
