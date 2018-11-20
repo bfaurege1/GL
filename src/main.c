e36710c7040edae176bd538abe83ea6a9dbb0b13
baptiste faure <baptiste.faure@ge1.iut-cachan.u-psud.fr>
2018-11-20 09:46:29 +0100

    debut exo56

--- src/main.c
+++ src/main.c
@@ -5,5 +5,48 @@
 
 int main(void)
 {
+	unsigned char bin[10]={25,10,140,75,56,89,220,5,207,184};
+	short int i,seuil=120;
+	
+	for (i=0 ; i<10 ; i=i+1)
+	{
+		if (bin[i] < seuil)
+			
+			bin[i] =0;
+		else
+			bin[i]=1;
+	
+	}
+	
+	
     return 0;
 }
+
+
+int main (void)
+{
+	sat[10]
+	
+	
+	
+	
+	
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
