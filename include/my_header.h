42d38f09ee7125d4eb823e852c686ace9dd5e6fb
bfaurege1 <baptiste.faure@u-psud.fr>
2018-11-27 19:26:04 +0100

    J'ai fini exo58

--- include/my_header.h
+++ include/my_header.h
@@ -4,6 +4,44 @@
 #ifndef MY_HEADER_H
 #define MY_HEADER_H
 
+#include "TP_lib_suite.h" 
 
+
+short score(void);
+
+short score(void)
+{
+	short comb_joueur[6] = {5,15,21,33,46,37} ;
+	short comb_gagnante[6];
+	short j = 0, i, x , numbon=0;
+	
+	
+
+	initialiserTirage();
+	for (x=0 ; x<6 ; x++)
+	{
+		comb_gagnante[x] = tirerNumero();
+		
+	}
+	
+	while ( j<6)
+	{
+		for (i=0 ; i<6 ; i++)
+		{
+			if (comb_joueur[j] == comb_gagnante[i])
+			{
+				
+				numbon = numbon+1 ;
+			}
+			
+		}
+		
+	j++;
+	
+	}
+
+    return numbon;
+	
+}
 
 #endif
--- src/main.c
+++ src/main.c
@@ -3,8 +3,24 @@
 
 #include "my_header.h"
 
+
 int main(void)
 {
+	short scoremax=0, k, a;
+	
+	
+	
+	for (k=0; k<100 ; k++)
+	{
+		a = score();
+		
+		if (a>scoremax)
+			scoremax = a;
+		else if (a==6)
+			k = 100;
+			
+	}
+
     return 0;
 	
 }
