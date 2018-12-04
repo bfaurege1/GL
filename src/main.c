e69bc332002aab02c5f5d14eb2e5193cd52f6096
baptiste faure <baptiste.faure@ge1.iut-cachan.u-psud.fr>
2018-12-04 09:36:33 +0100

    exo 79 terminé

--- src/main.c
+++ src/main.c
@@ -2,8 +2,28 @@
 //
 
 #include "my_header.h"
+#include <userint.h>
+#include <ansi_c.h>
+
 
 int main(void)
 {
+	
+	  char prenom[20], nom[20];
+	  long anneenais;
+	  char libelle[100];
+	  
+	  printf("Nom:") ;
+	  scanf("%s",nom)  ;
+	  printf("Prenom:");
+	  scanf("%s",prenom) ;
+	  printf("Annee de Naissance:");
+	  scanf("%u",&anneenais);
+	  
+	  sprintf(libelle,"je m'appelle %s %s et j'ai %u ans",nom ,prenom, 2018-anneenais);
+	  MessagePopup("exo79",libelle);
+	
+	
+	
     return 0;
 }
